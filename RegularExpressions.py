634431dcf7a71df8e46f3b8728ec6bf5ff1b2597
Bhupendra Singh <40202663+BS081997@users.noreply.github.com>
2018-06-20 21:40:03 +0530

    Update RegularExpressions.py

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -7,16 +7,16 @@ import re
 print(re.search("meet .*smile",quote_text))
 
 
-#__For search particular word
-
-match=re.search("found",quote_text)
-print(type(match))
-
-
 #__Method 2
 
 a=list(re.finditer("Let .*smile",quote_text))
 print(a)
+
+
+#__For search particular word
+
+match=re.search("found",quote_text)
+print(type(match))
 
 
 #__For get_number_of_occurrences
@@ -25,4 +25,16 @@ def get_number_of_occurrences(string,substring):
     findings=list(re.finditer(substring,string))
     return len(findings)
 print(get_number_of_occurrences("Let us always meet each other with smile,for the smile is the beginning of love","smile"))
+
+
+a=re.search(r'..d','food baar')
+print(a)
+
+
+
+def is_valid_Python_variable_name(string):
+    found = re.match("[_A-Za-z][_a-zA-Z0-9]*",string)
+    return  bool(found)
+print(is_valid_Python_variable_name("the"))
+
 
